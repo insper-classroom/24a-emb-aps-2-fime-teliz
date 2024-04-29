fe1bc070cd2f9c476a561c71f0838cfaefbee4b0
leonardopaloschi <leonardopaloschi@outlook.com>
2024-04-29 17:18:32 -0300

    python certinho

--- aps2-emb.py
+++ aps2-emb.py
@@ -1,11 +1,11 @@
 import serial
+import pydirectinput
 import pyautogui
 
-ser = serial.Serial('COM11', 115200)
 stateup = 1
-
+ser = serial.Serial('COM11', 115200)
 try:
-    pyautogui.PAUSE = 0
+    pydirectinput.PAUSE = 0
     # sync package
     while True:
         # print('Waiting for sync package...')
@@ -20,61 +20,63 @@ try:
             if data == b'\x01':
                 data = ser.read(2)
                 value = int.from_bytes(data, byteorder='big', signed=True)
-                #print(f"X: {value}")
-                #pyautogui.move(value, 0)
+                print(f"X: {value}")
+                value *=-10
+                pydirectinput.move(value, 0)
             if data == b'\x02':
                 data = ser.read(1) 
                 value = int.from_bytes(data, byteorder='big', signed=True)
                 if value == 1 and stateup == 1:
-                    pyautogui.mouseDown()
+                    pydirectinput.mouseDown()
                     print(f"click{value}")
-                    state = 0
+                    stateup = 0
                 elif value == 0:
-                    pyautogui.mouseUp()
-                    state = 1
-                    print(f"soltou click{value}")
+                    pydirectinput.mouseUp()
+                    stateup = 1
             if data == b'\x03':
                 data = ser.read(1)
                 value = int.from_bytes(data, byteorder='big', signed=True)
-                if value == 1:
+                if value == 0:
                     data = ser.read()
                     value = int.from_bytes(data, byteorder='big', signed=True)
                     if value == 2:    
-                        pyautogui.keyDown('d')
-                        pyautogui.keyUp('a')
+                        pydirectinput.keyDown('d')
+                        pydirectinput.keyUp('a')
                     elif value == -2:
-                        pyautogui.keyDown('a')
-                        pyautogui.keyUp('d')
+                        pydirectinput.keyDown('a')
+                        pydirectinput.keyUp('d')
                     elif value == 0:
-                        pyautogui.keyUp('d')
-                        pyautogui.keyUp('a')
-                elif value == 0:
+                        pydirectinput.keyUp('d')
+                        pydirectinput.keyUp('a')
+                elif value == 1:
                     data = ser.read(1)
                     value = int.from_bytes(data, byteorder='big', signed=True)
                     if value == 2:
-                        pyautogui.keyDown('w')
-                        pyautogui.keyUp('s')
+                        pydirectinput.keyDown('w')
+                        pydirectinput.keyUp('s')
                     elif value == -2:
-                        pyautogui.keyDown('s')
-                        pyautogui.keyUp('w')
+                        pydirectinput.keyDown('s')
+                        pydirectinput.keyUp('w')
                     elif value == 0:
-                        pyautogui.keyUp('w')
-                        pyautogui.keyUp('s')
+                        pydirectinput.keyUp('w')
+                        pydirectinput.keyUp('s')
             if data == b'\x04':
                 data = ser.read(1)
                 value = int.from_bytes(data, byteorder='big', signed=True)
                 if value == 1:
-                    pyautogui.mouseDown(button='right')
+                    pydirectinput.mouseDown(button='right')
                 else:
-                    pyautogui.mouseUp(button='right')
+                    pydirectinput.mouseUp(button='right')
 
             if data == b'\x05':
                 data = ser.read(1)
                 value = int.from_bytes(data, byteorder='big', signed=True)
                 if value == 1:
-                    pyautogui.mouseDown()
-                else:
-                    pyautogui.mouseUp()
+                    pydirectinput.mouseDown()
+                    print("abaixou")
+                elif value == 0:
+                    pydirectinput.mouseUp()
+                    print("levantou")
             if data == b'\x06':
                 data = ser.read(1)
                 value = int.from_bytes(data, byteorder='big', signed=True)
